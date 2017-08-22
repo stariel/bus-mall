ed57767d1302cd39ed7a4e3b85b2f9732d0beaf2
stariel <ariel.altaras@gmail.com>
2017-08-21 20:07:28 -0700

    code planning

---
+++ app.js
@@ -0,0 +1,8 @@
+// Code Planning
+// 1. Create HTML slots for photos to appear
+// 2. Create constructor function and push all objects to an array
+// 3. Create function to randomize images - global variables for pictures 1, 2, and 3
+// 4. Ensure that variables are reset at beginning of function, and that function repeats until picture 2 is not the same as 1, and 3 is not the same as 1 or 2
+// 5. Make sure on further iterations that pictures do not match any of the pictures from the previous set - alternating set of variables?
+// 6. Set up event listener to "listen" for clicks on each image
+// 7. "Score" how many times a picture is displayed (variable in constructor function?) and how many times it is clicked.
