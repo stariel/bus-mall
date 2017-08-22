4798194a3670344a3f7e98c59991f50804fc5e62
stariel <ariel.altaras@gmail.com>
2017-08-22 08:26:07 -0700

    created constructor function and html photo spots

--- app.js
+++ app.js
@@ -6,3 +6,11 @@
 // 5. Make sure on further iterations that pictures do not match any of the pictures from the previous set - alternating set of variables?
 // 6. Set up event listener to "listen" for clicks on each image
 // 7. "Score" how many times a picture is displayed (variable in constructor function?) and how many times it is clicked.
+
+function TestItem (name, filePath, itemID) {
+  this.name = name;
+  this.filePath = filePath;
+  this.itemID = itemID;
+  this.timesShown = 0;
+  this.timesVoted = 0;
+}
