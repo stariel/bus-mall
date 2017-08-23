7b1797c6d7920acdc18de39dd1d1dd22769e52cd
stariel <ariel.altaras@gmail.com>
2017-08-22 17:48:55 -0700

    working on randomization of pictures

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 'use strict';
 // Code Planning
 // 1. Create HTML slots for photos to appear
-// 4. Ensure that variables are reset at beginning of function, and that function repeats until picture 2 is not the same as 1, and 3 is not the same as 1 or 2
-// 5. Make sure on further iterations that pictures do not match any of the pictures from the previous set - alternating set of variables?
+
 // 6. Set up event listener to "listen" for clicks on each image
 // 7. "Score" how many times a picture is displayed (variable in constructor function?) and how many times it is clicked.
 
@@ -41,6 +40,8 @@ var waterCan = new TestItem ('self-watering can', 'img/water-can.jpg', 'waterCan
 var wineGlass = new TestItem ('wine glass', 'img/wine-glass.jpg', 'wineGlass');
 
 // 3. Create function to randomize images - global variables for pictures 1, 2, and 3
+// 4. Ensure that variables are reset at beginning of function, and that function repeats until picture 2 is not the same as 1, and 3 is not the same as 1 or 2
+// 5. Make sure on further iterations that pictures do not match any of the pictures from the previous set - alternating set of variables?
 
 var p1 = 0;
 var p2 = 0;
@@ -49,7 +50,7 @@ var previousPics = [];
 
 var randomPhoto = function() {
   p1 = Math.floor(Math.random() * itemArray.length);
-  while (previousPics.includes(p1) == true) {
+  while (previousPics.includes(p1)) {
     p1 = Math.floor(Math.random() * itemArray.length);
   }
   p2 = Math.floor(Math.random() * itemArray.length);
